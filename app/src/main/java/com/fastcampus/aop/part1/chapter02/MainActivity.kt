93159cc64499e615aa9c3138aa477cab7d3bc260
du2lee <dh151446@gmail.com>
2021-06-24 13:08:58 +0900

    hello world

---
+++ app/src/main/java/com/fastcampus/aop/part1/chapter02/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fastcampus.aop.part1.chapter02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_main)
+    }
+}
